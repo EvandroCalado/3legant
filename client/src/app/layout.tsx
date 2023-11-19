cb6554eb64a44166e472d03de4ee44616cbcbaec
Evandro Calado <evandrocalado07@gmail.com>
2023-11-19 20:07:34 -0300

    🐳 chore(providers): add providers

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Poppins } from 'next/font/google';
 import '../styles/globals.css';
+import Providers from '../utils/providers';
 
-const inter = Inter({ subsets: ['latin'] });
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+});
 
 export const metadata: Metadata = {
   title: 'Boilerplate Next',
@@ -16,9 +20,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>
-        <main>{children}</main>
-      </body>
+      <Providers>
+        <body className={poppins.className}>
+          <main>{children}</main>
+        </body>
+      </Providers>
     </html>
   );
 }
