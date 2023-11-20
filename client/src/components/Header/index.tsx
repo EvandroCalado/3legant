87a33e52a7ef27ae44a4bdaac6e526a173426a4a
Evandro Calado <evandrocalado07@gmail.com>
2023-11-20 19:17:23 -0300

    ✨ feat(components): create header

---
+++ client/src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import Logo from '../Logo';
+import MenuIcons from '../MenuIcons';
+import MenuLinks from '../MenuLinks';
+import MenuMobile from '../MenuMobile';
+
+export default function Header() {
+  return (
+    <header className="container mx-auto flex items-center justify-between px-4 py-4">
+      <div className="flex items-center gap-4">
+        <MenuMobile />
+        <Logo />
+      </div>
+      <MenuLinks />
+      <MenuIcons />
+    </header>
+  );
+}
