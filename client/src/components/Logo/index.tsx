4303588c91ee51c7046ead14aad07246fb7fbca9
Evandro Calado <evandrocalado07@gmail.com>
2023-11-21 09:08:14 -0300

    🌈 style(styles): fix font size

--- client/src/components/Logo/index.tsx
+++ client/src/components/Logo/index.tsx
@@ -1,7 +1,7 @@
 export default function Logo() {
   return (
     <div>
-      <h3 className="text-[24px] font-medium">3legant.</h3>
+      <h3 className="text-base font-medium sm:text-[24px]">3legant.</h3>
     </div>
   );
 }
