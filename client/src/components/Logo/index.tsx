1520f9da143d55063842583ffc7137ab9c3ff539
Evandro Calado <evandrocalado07@gmail.com>
2023-11-22 23:05:11 -0300

    🌈 style(styles): add tw merge

--- client/src/components/Logo/index.tsx
+++ client/src/components/Logo/index.tsx
@@ -1,7 +1,17 @@
-export default function Logo() {
+import { twMerge } from 'tailwind-merge';
+
+export type LogoProps = {
+  className?: string;
+};
+
+export default function Logo({ className }: LogoProps) {
   return (
     <div>
-      <h3 className="text-base font-medium sm:text-[24px]">3legant.</h3>
+      <h3
+        className={twMerge('text-base font-medium sm:text-[24px]', className)}
+      >
+        3legant.
+      </h3>
     </div>
   );
 }
