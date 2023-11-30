b2427c2377596e546ae8c378dabe76344eef55ec
Evandro Calado <evandrocalado07@gmail.com>
2023-11-30 20:18:35 -0300

    🐳 chore(icons): add icons

---
+++ client/src/icons/rows.tsx
@@ -0,0 +1,19 @@
+export default function Rows() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="true"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="lucide lucide-stretch-horizontal"
+    >
+      <rect width="20" height="6" x="2" y="4" rx="2" />
+      <rect width="20" height="6" x="2" y="14" rx="2" />
+    </svg>
+  );
+}
