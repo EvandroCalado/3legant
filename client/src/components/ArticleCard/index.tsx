7d0c957ffc2d47f836b4b91b072630fae56ed4f5
Evandro Calado <evandrocalado07@gmail.com>
2023-11-30 20:26:25 -0300

    ✨ feat(components): grid and rows mode

--- client/src/components/ArticleCard/index.tsx
+++ client/src/components/ArticleCard/index.tsx
@@ -5,28 +5,45 @@ export type ArticleCardProps = {
   image: string;
   title: string;
   slug: string;
+  viewMode?: 'grid' | 'rows';
 };
 
-export default function ArticleCard({ image, title, slug }: ArticleCardProps) {
+export default function ArticleCard({
+  image,
+  title,
+  slug,
+  viewMode,
+}: ArticleCardProps) {
   return (
-    <div className="flex w-full flex-col">
-      <Image src={image} alt={title} width={500} height={450} />
+    <div
+      className={`${
+        viewMode === 'rows' ? 'flex-row gap-4' : 'flex-col'
+      } flex w-full`}
+    >
+      <Image
+        src={image}
+        alt={title}
+        width={viewMode === 'rows' ? 300 : 500}
+        height={viewMode === 'rows' ? 300 : 450}
+        className={`${viewMode === 'rows' ? 'flex-1' : ''}`}
+      />
 
-      <h2 className="mb-2 mt-4 text-xl font-semibold">{title}</h2>
-
-      <Link
-        href={`/blog/${slug}`}
-        className="flex w-[83px] items-center border-b border-primary text-sm text-primary [&>img]:ml-1 [&>img]:hover:ml-2"
-      >
-        Ler mais{' '}
-        <Image
-          src={'/icons/arrow-black.svg'}
-          alt="Arrow"
-          width={24}
-          height={24}
-          className="h-4 w-4 text-primary duration-150"
-        />
-      </Link>
+      <div className={`${viewMode === 'rows' ? 'flex-1' : ''}`}>
+        <h2 className="mb-2 mt-4 text-xl font-semibold">{title}</h2>
+        <Link
+          href={`/blog/${slug}`}
+          className={`flex w-[83px] items-center border-b border-primary text-sm text-primary [&>img]:ml-1 [&>img]:hover:ml-2`}
+        >
+          Ler mais{' '}
+          <Image
+            src={'/icons/arrow-black.svg'}
+            alt="Arrow"
+            width={24}
+            height={24}
+            className="h-4 w-4 text-primary duration-150"
+          />
+        </Link>
+      </div>
     </div>
   );
 }
--- client/src/components/Articles/index.tsx
+++ client/src/components/Articles/index.tsx
@@ -1,21 +1,34 @@
 import ArticleCard, { ArticleCardProps } from '../ArticleCard';
 
 export type ArticlesProps = {
+  title?: string;
+  viewMode?: 'grid' | 'rows';
   articles: ArticleCardProps[];
 };
 
-export default function Articles({ articles }: ArticlesProps) {
+export default function Articles({
+  articles,
+  title,
+  viewMode = 'grid',
+}: ArticlesProps) {
   return (
-    <article className="container mx-auto my-20 px-4">
-      <h2 className="mb-8 text-2xl font-semibold">Artigos</h2>
+    <article className="container mx-auto my-20">
+      <h2 className="mb-8 text-2xl font-semibold">{title}</h2>
 
-      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+      <div
+        className={`${
+          viewMode === 'grid'
+            ? 'grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3'
+            : 'grid grid-cols-1 gap-8 md:grid-cols-2'
+        }`}
+      >
         {articles.map((article) => (
           <ArticleCard
             key={article.title}
             image={article.image}
             title={article.title}
             slug={article.slug}
+            viewMode={viewMode}
           />
         ))}
       </div>
