542a660f8be72cba2c6a22c9aaf51e98bf30c40e
Evandro Calado <evandrocalado07@gmail.com>
2023-11-30 23:40:58 -0300

    ✨ feat(components): add user session

--- client/src/components/MenuIcons/index.tsx
+++ client/src/components/MenuIcons/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { useState } from 'react';
 import User from '../../icons/user';
@@ -8,6 +9,7 @@ import MenuSearch from '../MenuSearch';
 
 export default function MenuIcons() {
   const [open, setOpen] = useState(false);
+  const session = useSession();
 
   return (
     <div className="flex items-center space-x-4">
@@ -23,12 +25,26 @@ export default function MenuIcons() {
             open ? 'visible top-8' : 'invisible top-4'
           } absolute right-0 z-30 flex flex-col gap-4 rounded-md border bg-white p-4 text-neutral-400 shadow-xl duration-300`}
         >
-          <Link href={'/signin'} className="duration-150 hover:text-primary">
-            Entrar
-          </Link>
-          <Link href={'/account'} className="duration-150 hover:text-primary">
-            Conta
-          </Link>
+          {session.status === 'unauthenticated' ? (
+            <Link href={'/signin'} className="duration-150 hover:text-primary">
+              Entrar
+            </Link>
+          ) : (
+            <>
+              <button
+                className="duration-150 hover:text-primary"
+                onClick={() => signOut()}
+              >
+                Sair
+              </button>
+              <Link
+                href={'/account'}
+                className="duration-150 hover:text-primary"
+              >
+                Conta
+              </Link>
+            </>
+          )}
         </div>
       </span>
 
